8ac36fd7a5b2215281342619370249d8ba30c964
Advika Y <advika3211@gmail.com>
2025-12-29 15:54:01 +0530

    Update orbit_simulator.py

--- orbit_simulator.py
+++ orbit_simulator.py
@@ -1,5 +1,4 @@
-# Simple Orbit Simulator using Turtle
-# Concept: Circular orbital motion using basic trigonometry
+
 
 import turtle
 import math
@@ -26,19 +25,19 @@ planet.color("cyan")
 planet.shapesize(0.7)
 planet.penup()
 
-# -------------------- Info Display --------------------
+
 info = turtle.Turtle()
 info.hideturtle()
 info.color("white")
 info.penup()
 info.goto(-300, 250)
 
-# Orbit Parameters
-radius = 150      # distance from sun
-angle = 0         # starting angle
-speed = 0.05      # speed of orbit
 
-# -------------------- Update Info Text --------------------
+radius = 150      #distance from sun
+angle = 0         #starting angle
+speed = 0.05      #speed of orbit
+
+
 def update_info():
     info.clear()
     circumference = 2 * math.pi * radius
@@ -50,7 +49,7 @@ def update_info():
         font=("Arial", 12, "normal")
     )
 
-#Radius Control Functions 
+
 def increase_radius():
     global radius
     radius += 10
@@ -69,14 +68,14 @@ def decrease_speed():
     if speed > 0.01:
         speed -= 0.01
 
-# Keyboard Binding
+
 screen.listen()
 screen.onkey(increase_radius, "Up")
 screen.onkey(decrease_radius, "Down")
 screen.onkey(increase_speed, "Right")
 screen.onkey(decrease_speed, "Left")
 
-# Orbit Loop
+
 while True:
     x = radius * math.cos(angle)
     y = radius * math.sin(angle)
